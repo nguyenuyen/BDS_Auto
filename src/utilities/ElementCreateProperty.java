b7bed8577825b913f0eaf60cdde9abaf581823ea
NguyenUyen <uyenuyen.02061997@gmail.com>
2020-12-08 18:42:48 +0700

    GET EXPATH

--- src/utilities/ElementCreateProperty.java
+++ src/utilities/ElementCreateProperty.java
@@ -3,43 +3,43 @@ public class ElementCreateProperty {
     public static final String TYPE_RENT = "//div[@class='par-layout']/div[contains(@class,'ng-star-inserted')][2]/span";
     public static final String IS_BUILDING_ONE = "//input[@type='radio'][1]";
     public static final String IS_BUILDING_MANY = "//input[@type='radio'][2]";
+
     public static final String BUY_TIME = "//input[@type='date'][1]";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
-    public static final String USERNAME = "//input[@formcontrolname='username']";
+
+    public static final String TITLE = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[1]//input";
+    public static final String PROPERTY_NAME = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[2]//input";
+
+    //if isBuilding = 1 then roomNumber
+    //if isBuilding = 2 then currentLocation
+    public static final String ROOM_NUMBER = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[3]//input";
+    public static final String CURRENT_LOCATION = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[4]//input";
+    public static final String BED_ROOM = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[5]//input";
+    public static final String BATH_ROOM = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[6]//input";
+    public static final String SELLING_PRICE = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[7]//input";
+    public static final String SELECT_PRICE = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[7]//select";
+    public static final String LOCAL_CURRENCY = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[8]//input";
+    public static final String PROFIT = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[9]//input";
+    public static final String PROPERTY_TYPE = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[10]//select";
+    public static final String TRAFFIC = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[11]//input";
+    public static final String NUMBER_FLOOR_BUILDING = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[12]//input";
+    public static final String TOTAL_DEPARTMENT = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[13]//input";
+    public static final String YEAR_COMPLETION_CONTRUCTION = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][1]/div[14]//input";
+
+    public static final String MOVE_DATE = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[1]//input";
+    public static final String LODGING_OPERATION = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[2]//select";
+    public static final String PURPOSE = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[3]//select";
+    public static final String AREA = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[4]//input";
+    public static final String MANAGER_FEE = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[5]//input";
+    public static final String REPAIR_RESERVE_FUND = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[6]//input";
+    public static final String PARKING_FEE = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[7]//input";
+    public static final String OWNER_SHIP = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[8]//input";
+    public static final String BUILDING_STRUCTURE = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[9]//input";
+    public static final String COMPANY_INFOMATION = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[10]//input";
+    public static final String SELLER = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[11]//input";
+    public static final String MOVE_IN = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[12]//input";
+    public static final String SELLER_INFORMATION = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[13]//textarea";
+    public static final String SELLER_LOGO = "//body//div[@class='property-layout']//div[4]//div[@class='container-fluid']//div[@class='col-md-6'][2]/div[14]//span";
+
+
 
 }
